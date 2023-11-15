7cdc38dbe53f89388fea500c0b6fd7014ebf2c1a
Simon Henderson <simonhenderson100@gmail.com>
2023-11-15 12:42:05 -0800

    Save Push

--- Vr project, Kyan/Assets/Hammer.cs
+++ Vr project, Kyan/Assets/Hammer.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public enum State { Throw,Return,Idle }
 public class Hammer : MonoBehaviour
 {
-    public float hammerspeed;
+    public float hammerSpeed;
+    public float returnSpeed;
+    public float returnRange;
     Rigidbody rb;
     State state;
     public Transform playerHand;
+
     void Start()
     {
         state = State.Idle;
@@ -18,11 +22,11 @@ public class Hammer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       switch (state)
+        switch (state)
         {
             case State.Throw:
                 {
-                    rb.velocity = rb.velocity.normalized * hammerspeed;
+                    rb.velocity = rb.velocity.normalized * hammerSpeed;
                 }
                 break;
 
@@ -34,16 +38,16 @@ public class Hammer : MonoBehaviour
 
             case State.Return:
                 {
-                    if (Vector3.SqrMagnitude(playerHand.position - transform.position) < 16)
+                    if (Vector3.SqrMagnitude(playerHand.position - transform.position) < returnRange)
                     {
                         Vector3 dir = (playerHand.position - transform.position).normalized;
-                        rb.velocity = dir * hammerspeed;
+                        rb.velocity = dir * returnSpeed;
                     }
                     else
                     {
                         state = State.Idle;
+                        UnityEngine.Debug.Log("Hammer too far away");
                     }
-                    
                 }
                 break;
 
@@ -52,25 +56,19 @@ public class Hammer : MonoBehaviour
             
         }   
     }
+
     public void ThrowHammer()
     {
         state = State.Throw;
     }
+
     public void ReturnHammer()
     {
         state = State.Return;
     }
+
     public void IdleHammer()
     {
         state = State.Idle;
     }
-
-
-    }
-    
-
-
-    
-        
-    
-
+}
